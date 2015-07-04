704db7c274482952ebbb920b0e325ffe3a05198f
Michael Williams <draringi@draringi.net>
2015-07-04 17:30:49 -0400

    More work on urls

---
+++ aniprofile/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from aniprofile.views import ProfileView
+
+urlpatterns = patterns('',
+    url(r'^profile/$', ProfileView.as_view(), name="self_user_view"),
+    url(r'^profile/(?P<pk>\d+)/$', ProfileView.as_view(), name="user_view"),
+    #url(r'^register/$','MAC_profile.views.registration',name="register"),
+)
--- aniprofile/views.py
+++ aniprofile/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from aniprofile.models import Profile, Exec
+from django.core.exceptions import PermissionDenied
 
 # Create your views here.
+
+class ProfileView(DetailView):
+    model = Profile
+    template_name = "aniprofile/profile.html"
+    context_object_name = "usr"
+    
+    def get_object(self, queryset=None):
+        try:
+            pk = self.kwargs["pk"]
+        except:
+            if self.request.user.is_authenticated:
+                pk = self.request.user.id
+            else:
+                raise PermissionDenied
+        object = get_object_or_404(queryset ,pk)
+        return object
