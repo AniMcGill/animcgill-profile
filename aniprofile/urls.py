aeba02f538c9eed0ac21e6a4308c27108059358e
Michael Williams <draringi@draringi.net>
2015-09-02 16:53:37 -0400

    Enable exec list in urls

--- aniprofile/urls.py
+++ aniprofile/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-from aniprofile.views import ProfileView
+from aniprofile.views import ProfileView, ExecList
 
 urlpatterns = patterns('',
     url(r'^profile/$', ProfileView.as_view(), name="self_user_view"),
     url(r'^profile/(?P<pk>\d+)/$', ProfileView.as_view(), name="user_view"),
     url(r'^register/$', 'aniprofile.views.registration', name="register"),
+    url(r'^execs/$', ExecList.as_view(), name="exec_list"),
     url('^', include('django.contrib.auth.urls'))
 )
